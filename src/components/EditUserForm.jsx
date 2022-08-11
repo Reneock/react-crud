95b74701956c42283f5256cf68749ba37e3cec4f
Irene Ockrah <ostrene33@gmail.com>
2022-08-11 05:43:03 -0700

    introducing edit and delete components and props

--- src/components/EditUserForm.jsx
+++ src/components/EditUserForm.jsx
@@ -1,11 +1,55 @@
 import React, { Component } from 'react';
+import { Form, Button} from 'react-bootstrap';
 
 class EditUserForm extends Component {
+  constructor(props){
+    super(props);
+    this.state ={name: props.userInfo.name,
+     email: props.userInfo.email,
+     gen: props.userInfo.gen,
+     id:props.userInfo.id
+    }
+  }
+
+  handleChange=(e)=>{
+    e.preventDefault();
+    this.setState({
+      [e.target.name] : e.target.value
+    });
+  };
+
+  handleSubmit=(e)=>{
+    e.preventDefault();
+    //this.props.addUser(this.state); to edit below code is introduced
+    this.props.editUser(this.state.id, this.state);
+    this.setState({name:"", email:"", gen: ""});
+    this.props.closeModal();
+  };
+
+
   render() {
     return (
-      <div>
-        Edit Form
-      </div>
+
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="text" placeholder="Enter name" value={this.state.name} name="name" onChange={this.handleChange}/>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" value={this.state.email} name="email" onChange={this.handleChange}/>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Gen</Form.Label>
+            <Form.Control type="number" placeholder="Enter Gen" value={this.state.gen} name="gen" onChange={this.handleChange}/>
+          </Form.Group>
+          
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
     );
   }
 }
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,22 +1,49 @@
-import React from 'react';
-import { Card, Col, Button } from 'react-bootstrap';
+import React, {useState} from 'react';
+import { Card, Col, Button, Modal } from 'react-bootstrap';
+import EditUserForm from './EditUserForm';
 
 const User = (props) => {
+  
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleDelete = (e) => {
+    e.preventDefault();
+    props.deleteUser(props.userInfo.id);
+  };
+
   return (
-    <Col md="4" style={{marginBottom: "1rem"}}>  
-      <Card style={{backgroundColor:"purple", color:"white"}}>
-        <Card.Body>
-          <Card.Subtitle className="mb-2 text-muted">Codetrain User</Card.Subtitle>
-          <Card.Title>{props.userInfo.name}</Card.Title>
-          <Card.Text>
-            <p>Email: {props.userInfo.email}</p>
-            <p>Gen: {props.userInfo.gen}</p>
-          </Card.Text>
-          <Button style={{width: "75px"}} href="#" variant="primary">Edit</Button>
-          <Button style={{marginLeft:"10px"}}href="#" variant="danger">Delete</Button>
-        </Card.Body>
-      </Card>
-    </Col>
+    <>
+      <Modal show={show} onHide={handleClose} animation={false}>
+        <Modal.Header closeButton>
+          <Modal.Title>Edit User</Modal.Title>
+        </Modal.Header>
+        <Modal.Body> 
+          <EditUserForm userInfo={props.userInfo} editUser={props.editUser} closeModal={handleClose}/> 
+        </Modal.Body>
+      </Modal>
+
+      <Col md="4" style={{ marginBottom: "1rem" }}>
+        <Card style={{ backgroundColor: "purple", color: "white" }}>
+          <Card.Body>
+            <Card.Subtitle className="mb-2 text-muted">Codetrain User</Card.Subtitle>
+            <Card.Title>{props.userInfo.name}</Card.Title>
+            <Card.Text>
+              <p>Email: {props.userInfo.email}</p>
+              <p>Gen: {props.userInfo.gen}</p>
+            </Card.Text>
+            <Card.Link href="#">
+              <Button style={{ width: "75px" }} href="#" variant="primary" onClick={handleShow}>Edit</Button>
+            </Card.Link>
+            <Card.Link href="#">
+              <Button style={{ marginLeft: "10px" }} href="#" variant="danger" onClick={handleDelete}>Delete</Button>
+            </Card.Link>
+          </Card.Body>
+        </Card>
+      </Col>
+    </>
   );
 };
 
